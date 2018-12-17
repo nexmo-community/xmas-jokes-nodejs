b17ae9661de3cbf90d82fae1d077812254dcf283
Martyn Davies <martynrdavies@gmail.com>
2018-12-17 18:34:36 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,80 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const Nexmo = require('nexmo');
+const PORT = process.env.PORT || 3000;
+const DEV = process.env.NODE_ENV !== 'production';
+const jokes = require('./src/jokes');
+
+if (DEV) {
+  require('dotenv').config();
+}
+
+const {
+  NEXMO_API_KEY,
+  NEXMO_API_SECRET,
+  NEXMO_APPLICATION_ID,
+  NEXMO_APPLICATION_PRIVATE_KEY,
+  NEXMO_FROM_NUMBER
+} = process.env;
+
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+const nexmo = new Nexmo({
+  apiKey: NEXMO_API_KEY,
+  apiSecret: NEXMO_API_SECRET,
+  applicationId: NEXMO_APPLICATION_ID,
+  privateKey: NEXMO_APPLICATION_PRIVATE_KEY
+});
+
+const reply = async (number, cb) => {
+  const joke = () => {
+    let rand = Math.floor(Math.random() * jokes.length);
+    return jokes[rand].line;
+  };
+  nexmo.channel.send(
+    { type: 'sms', number: number },
+    { type: 'sms', number: NEXMO_FROM_NUMBER },
+    {
+      content: {
+        type: 'text',
+        text: 'Quick, say this: ' + joke()
+      }
+    },
+    (err, data) => {
+      if (data) {
+        cb({ sent: true, data });
+      } else {
+        cb({ sent: false, err });
+      }
+    }
+  );
+};
+
+router.post('/inbound', async ctx => {
+  const { msisdn, text } = await ctx.request.body;
+  lowerText = text.toLowerCase();
+  if (lowerText === 'awkward' || lowerText === 'more') {
+    reply(msisdn, response => {
+      response.sent
+        ? console.log('Message sent')
+        : console.log('Message not sent');
+    });
+    ctx.status = 200;
+  } else {
+    console.log('Message not sent');
+    //ctx.status = 200;
+  }
+  //ctx.status = 200;
+});
+
+router.post('/status', async ctx => {
+  ctx.status = 200;
+});
+
+app.use(router.routes());
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ src/jokes.js
@@ -0,0 +1,38 @@
+module.exports = [
+  {
+    line:
+      "There's nothing like the joy on a kid's face when he first sees the PlayStation box containing the socks I got them for Christmas."
+  },
+  {
+    line:
+      "How do you know when Santa's in the room? You can sense his presents."
+  },
+  {
+    line: 'What do you call an elf who sings? A wrapper!'
+  },
+  {
+    line:
+      "Why are Christmas trees so fond of the past? Because the present's beneath them."
+  },
+  {
+    line:
+      "What do they sing at a snowman's birthday party? Freeze a jolly good fellow"
+  },
+  {
+    line: 'If you cross mistletoe with a duck, do you get a Christmas quacker?'
+  },
+  {
+    line: "If Santa's little helpers ate outside would they be dining elfresco?"
+  },
+  {
+    line: 'What is Santa’s favorite snack food? Crisp Pringles'
+  },
+  {
+    line:
+      'Why do mummies like Christmas so much? They’re into all the wrapping.'
+  },
+  {
+    line:
+      'What do you call an elf wearing ear muffs? Anything you want. They can’t hear you!'
+  }
+];
